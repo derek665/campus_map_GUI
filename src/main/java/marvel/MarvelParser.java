9475db5772638a43656d7cb7114a5fe8096916e4
Avidant Bhagat <avidant@cs.washington.edu>
2019-05-09 17:42:02 -0700

    Staff push of files

---
+++ src/main/java/marvel/MarvelParser.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright ©2019 Hal Perkins.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Spring Quarter 2019 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+package marvel;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/** Parser utility to load the Marvel Comics dataset. */
+public class MarvelParser {
+
+  /**
+   * Reads the Marvel Universe dataset. Each line of the input file contains a character name and a
+   * comic book the character appeared in, separated by a tab character
+   *
+   * @spec.requires filename is a valid file path
+   * @param filename the file that will be read
+   */
+  // TODO: Pick your return type and document it
+  public static void parseData(String filename) {
+    // TODO: Complete this method
+    // Hint: You might want to create a new class to use with the CSV Parser
+  }
+}
---
+++ src/test/java/marvel/specTest/MarvelTestDriver.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright ©2019 Hal Perkins.  All rights reserved.  Permission is
+ * hereby granted to students registered for University of Washington
+ * CSE 331 for use solely during Spring Quarter 2019 for purposes of
+ * the course.  No other use, copying, distribution, or modification
+ * is permitted without prior written consent. Copyrights for
+ * third-party components of this work must be honored.  Instructors
+ * interested in reusing these course materials should contact the
+ * author.
+ */
+
+package marvel.specTest;
+
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * This class implements a testing driver which reads test scripts from files for testing Graph, the
+ * Marvel parser, and your BFS algorithm.
+ */
+public class MarvelTestDriver {
+
+  public static void main(String args[]) {}
+
+  public MarvelTestDriver(Reader r, Writer w) {}
+
+  public void runTests() {}
+}
