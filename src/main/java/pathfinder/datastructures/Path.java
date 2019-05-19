0db44dfe1ac0de67de50778fb9e96070573d40fa
derek665 <derek665@cs.washington.edu>
2019-05-19 12:56:05 -0700

    update

--- src/main/java/pathfinder/datastructures/Path.java
+++ src/main/java/pathfinder/datastructures/Path.java
@@ -213,7 +213,7 @@ public class Path<Node> implements Iterable<Path<Node>.Segment> {
   /**
    * Segment represents a single segment as part of a longer, more complex path between objects.
    * Segments are immutable parts of a larger path that cannot be instantiated directly, and
-   * are created as part of larger paths by calling {@link Path#extend(Node, double)}.
+   * are created as part of larger paths by calling {@link Path#extend(Object, double)}
    */
   public class Segment {
 
