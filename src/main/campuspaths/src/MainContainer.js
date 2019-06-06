1f7de7918aedbc70c2c9d0995f36dddff58796fc
derek665 <derek66519@gmail.com>
2019-06-05 17:37:47 -0700

    update

---
+++ src/main/campuspaths/src/MainContainer.js
@@ -0,0 +1,71 @@
+/* Hold elements of the CampusPaths React APP */
+
+import React, {Component} from 'react';
+import Map from './Map.js';
+import {MenuItem, Select} from "@material-ui/core";
+import * as fetch from "node-fetch";
+
+class MainContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            start: "",
+            end: "",
+            path: ""
+        };
+    };
+
+    // get the list for the drop down menu for user to select
+    getBuildingList = () => {
+        let lst = [];
+        fetch("http://localhost:4567/buildings").then((res) => {
+            if (res.status !== 200) {
+                throw Error("Fetch failed");
+            }
+            return res.json();
+        }).then((resText) => {
+            let keys = Object.keys(resText);
+            keys.forEach((key) => {
+                let menuComponent = (<MenuItem value={key}>{resText[key]} </MenuItem>);
+                lst.push(menuComponent);
+            })
+        });
+        return lst;
+    };
+    // modify the start point when user selected one from the list
+    startChange = (event) => {
+        this.setState({start: event.target.value});
+    };
+
+    // modify the end point when user selected one from the list
+    endChange = (event) => {
+        this.setState({end: event.target.value});
+    };
+
+    reset = () => {
+        this.setState({start:"", end:""})
+    }
+
+    render() {
+        let menuItems = this.getBuildingList();
+        return (
+            <div>
+                <Map start={this.state.start} end={this.state.end} reset={this.reset}/>
+                <div className="center-text">
+                    Start: {this.state.start}
+                    &nbsp; &nbsp; &nbsp;
+                    End: {this.state.end}
+                    <div>
+                        <Select onChange={this.startChange} value={this.state.start}>{menuItems} </Select>
+                        &nbsp; &nbsp; &nbsp;
+                        <Select onChange={this.endChange} value={this.state.end}>{menuItems} </Select>
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
+
+}
+
+export default MainContainer;
--- src/main/campuspaths/src/Map.js
+++ src/main/campuspaths/src/Map.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import "./Map.css";
-import {MenuItem, Select} from "@material-ui/core";
 import * as fetch from "node-fetch";
-import Button from '@material-ui/core/Button';
+import Button from './Button.js';
 
 
 class Map extends Component {
@@ -11,44 +10,14 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        start: "",
-        end: "",
-        path: "",
-      };
+        path:""
+    };
     this.canvasReference = React.createRef();
     this.backgroundImage = new Image();
     this.backgroundImage.onload = () => {
       this.drawBackgroundImage();
     };
     this.backgroundImage.src = "campus_map.jpg";
-  };
-
-  // get the list for the drop down menu for user to select
-  getBuildingList = () => {
-      let lst = [];
-      fetch("http://localhost:4567/buildings").then((res) => {
-          if (res.status !== 200) {
-              throw Error("Fetch failed");
-          }
-          return res.json();
-      }).then((resText) => {
-          let keys = Object.keys(resText);
-          keys.forEach((key) => {
-              let menuComponent = (<MenuItem value={key}>{resText[key]} </MenuItem>);
-              lst.push(menuComponent);
-          })
-      });
-      return lst;
-  };
-
-  // modify the start point when user selected one from the list
-  startChange = (event) => {
-      this.setState({start: event.target.value});
-  };
-
-  // modify the end point when user selected one from the list
-  endChange = (event) => {
-    this.setState({end: event.target.value});
   };
 
   // draw the map on canvas as the background
@@ -65,8 +34,8 @@ class Map extends Component {
   // this function if called when the user clicked on the "Go" button, and draw the path with the endpoints,
   // if the endpoints are the same then it will alert the user and reset the endpoint
   getPath = () => {
-      if (this.state.start !== "" && this.state.end !== "" && this.state.start !== this.state.end) {
-          fetch("http://localhost:4567/findPath?start=" + this.state.start + "&end=" + this.state.end).then((res) => {
+      if (this.props.start !== "" && this.props.end !== "" && this.props.start !== this.props.end) {
+          fetch("http://localhost:4567/findPath?start=" + this.props.start + "&end=" + this.props.end).then((res) => {
               if (res.status !== 200) {
                   throw Error("Fetch failed");
               }
@@ -75,7 +44,7 @@ class Map extends Component {
               this.setState({path: resText.path});
               this.drawPath()
           });
-      } else if (this.state.start !== "" && this.state.end !== "") {
+      } else if (this.props.start !== "" && this.props.end !== "") {
           alert("Both end points are the same");
           this.clearHandler();
       }
@@ -99,31 +68,18 @@ class Map extends Component {
   // redraw the background and clear the lines, resetting the endpoints and path to ""
   clearHandler = () => {
       this.drawBackgroundImage();
-      this.setState({start: "" , end:"" , path:""});
+      this.props.reset();
   };
 
   render() {
     // that's set up to center the canvas for you. See Map.css for more details.
     // Make sure you set up the React references for the canvas correctly so you
     // can get the canvas object and call methods on it.
-      let menuItems = this.getBuildingList();
     return (
       <div className="canvasHolder">
           <canvas ref={this.canvasReference} width={this.backgroundImage.width} height={this.backgroundImage.height} />
-          <p>
-              Start: {this.state.start}
-              &nbsp; &nbsp; &nbsp;
-              End: {this.state.end}
-          </p>
-          <Select onChange={this.startChange} value={this.state.start}>{menuItems} </Select>
-          &nbsp; &nbsp; &nbsp;
-          <Select onChange={this.endChange} value={this.state.end}>{menuItems} </Select>
-          <div className="center-text button">
-              <Button variant="contained" color="primary" onClick={this.getPath}>Go </Button>
-          </div>
-          <div className="center-text button">
-              <Button variant="contained" color="secondary" onClick={this.clearHandler}>Clear Path </Button>
-          </div>
+          <Button color="primary" onClick={this.getPath} value="Find Path" />
+          <Button color="secondary" onClick={this.clearHandler} value="Clear Path" />
       </div>
     )
   }
